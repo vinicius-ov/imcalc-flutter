6ef9e2942c40bbf69bee59d3fc87f3c43849c697
Vinicius Valvassori <vinicius.ov@gmail.com>
2023-10-16 15:06:51 -0300

    Add list for IMCs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'model/person.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,46 +9,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Calculadora IMC'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +32,168 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<Person> history = List.empty(growable: true);
+  var nameTextController = TextEditingController(text: '');
+  var heightTextController = TextEditingController(text: '');
+  var weightTextController = TextEditingController(text: '');
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+            child: SingleChildScrollView(
+                child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxHeight: MediaQuery.of(context).size.height),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 10),
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: Row(children: [
+                              const SizedBox(
+                                width: 60,
+                                child: Text('Nome: '),
+                              ),
+                              Flexible(
+                                child: TextField(
+                                    decoration: const InputDecoration(
+                                        hintText: 'Insira seu nome',
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey)),
+                                    keyboardType: TextInputType.name,
+                                    controller: nameTextController),
+                              )
+                            ]),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 10),
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: Row(children: [
+                              const SizedBox(
+                                width: 60,
+                                child: Text('Altura: '),
+                              ),
+                              Flexible(
+                                child: TextField(
+                                    decoration: const InputDecoration(
+                                        hintText: 'Insira sua altura',
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey)),
+                                    keyboardType: TextInputType.number,
+                                    controller: heightTextController),
+                              )
+                            ]),
+                          ),
+                          Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 10),
+                            height: 50,
+                            alignment: Alignment.center,
+                            child: Row(children: [
+                              const SizedBox(
+                                width: 60,
+                                child: Text('Peso: '),
+                              ),
+                              Flexible(
+                                child: TextField(
+                                    decoration: const InputDecoration(
+                                        labelStyle: TextStyle(fontSize: 8),
+                                        hintText: 'Insira seu peso',
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey)),
+                                    keyboardType: TextInputType.number,
+                                    controller: weightTextController),
+                              )
+                            ]),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                String name = nameTextController.text;
+                                double height =
+                                    double.parse(heightTextController.text);
+                                double weight =
+                                    double.parse(weightTextController.text);
+
+                                if (name.isNotEmpty &&
+                                    height > 0 &&
+                                    weight > 0) {
+                                  Person person = Person(
+                                      name: name,
+                                      weight: weight,
+                                      height: height);
+                                  debugPrint(person.toString());
+                                  setState(() {
+                                    history.add(person);
+                                    nameTextController.text = '';
+                                    heightTextController.text = '';
+                                    weightTextController.text = '';
+                                    FocusManager.instance.primaryFocus
+                                        ?.unfocus();
+                                  });
+                                }
+                              },
+                              style: ButtonStyle(
+                                  shape: MaterialStateProperty.all(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10))),
+                                  backgroundColor: MaterialStateProperty.all(
+                                      const Color.fromARGB(255, 141, 79, 151))),
+                              child: const Text(
+                                "Calcular IMC",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400),
+                              )),
+                          const SizedBox(height: 10),
+                          const Divider(
+                            thickness: 1.0,
+                          ),
+                          Expanded(
+                              child: Scrollbar(
+                                  child: ListView.builder(
+                                      itemCount: history.length,
+                                      itemBuilder:
+                                          (BuildContext bc, int index) {
+                                        var person = history[index];
+                                        return Column(children: [
+                                          Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                Text('Nome: ${person.name}'),
+                                                Text(
+                                                    'Situação: ${person.getImcMessage()}'),
+                                              ]),
+                                          Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                Text(
+                                                    'Altura: ${person.getHeight()}'),
+                                                Text(
+                                                    'Peso: ${person.getWeight()}'),
+                                                Text(
+                                                    'IMC: ${person.getImcString()}'),
+                                              ]),
+                                          const Divider(
+                                            thickness: 3.0,
+                                          )
+                                        ]);
+                                      })))
+                        ]))
+                //This trailing comma makes auto-formatting nicer for build methods.
+                )));
   }
 }
